9cfea796d25871bf5bb16e031080c4c233b8923f
UIVsauce <>
2026-01-06 18:42:34 +0000

    While Loop

---
+++ loops/while_loop/Main.java
@@ -0,0 +1,21 @@
+package com.example;
+
+public class Main {
+    public static int sumFrom1ToN(int n) {
+        int sum = ___;
+        int current = ___;
+
+        while (___) {
+            sum = ___;
+            current = ___;
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        int result = sumFrom1ToN(n);
+        System.out.println("The sum of numbers from 1 to " + n + " is: " + result);
+    }
+}
